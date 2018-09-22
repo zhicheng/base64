b7982e58af179e9955a7f5e6c8517512501a279f
Zhicheng Wei <zhicheng@opensourceforge.net>
2018-09-22 13:35:32 +0800

    change return value to out length

--- base64.c
+++ base64.c
@@ -1,8 +1,8 @@
 /* This is a public domain base64 implementation written by WEI Zhicheng. */
 
-#include <stdio.h>
-
 #include "base64.h"
+
+#define BASE64_PAD '='
 
 /* BASE 64 encode table */
 static const char base64en[] = {
@@ -16,122 +16,148 @@ static const char base64en[] = {
 	'4', '5', '6', '7', '8', '9', '+', '/',
 };
 
-#define BASE64_PAD	'='
+/* ASCII order for BASE 64 decode, 255 in unused character */
+static const unsigned char base64de[] = {
+	/* nul, soh, stx, etx, eot, enq, ack, bel, */
+	   255, 255, 255, 255, 255, 255, 255, 255,
 
+	/*  bs,  ht,  nl,  vt,  np,  cr,  so,  si, */
+	   255, 255, 255, 255, 255, 255, 255, 255,
 
-#define BASE64DE_FIRST	'+'
-#define BASE64DE_LAST	'z'
-/* ASCII order for BASE 64 decode, -1 in unused character */
-static const signed char base64de[] = {
-	/* '+', ',', '-', '.', '/', '0', '1', '2', */ 
-	    62,  -1,  -1,  -1,  63,  52,  53,  54,
+	/* dle, dc1, dc2, dc3, dc4, nak, syn, etb, */
+	   255, 255, 255, 255, 255, 255, 255, 255,
 
-	/* '3', '4', '5', '6', '7', '8', '9', ':', */
-	    55,  56,  57,  58,  59,  60,  61,  -1,
+	/* can,  em, sub, esc,  fs,  gs,  rs,  us, */
+	   255, 255, 255, 255, 255, 255, 255, 255,
 
-	/* ';', '<', '=', '>', '?', '@', 'A', 'B', */
-	    -1,  -1,  -1,  -1,  -1,  -1,   0,   1, 
+	/*  sp, '!', '"', '#', '$', '%', '&', ''', */
+	   255, 255, 255, 255, 255, 255, 255, 255,
 
-	/* 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', */
-	     2,   3,   4,   5,   6,   7,   8,   9,
+	/* '(', ')', '*', '+', ',', '-', '.', '/', */
+	   255, 255, 255,  62, 255, 255, 255,  63,
 
-	/* 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', */ 
-	    10,  11,  12,  13,  14,  15,  16,  17,
+	/* '0', '1', '2', '3', '4', '5', '6', '7', */
+	    52,  53,  54,  55,  56,  57,  58,  59,
 
-	/* 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', */
-	    18,  19,  20,  21,  22,  23,  24,  25,
+	/* '8', '9', ':', ';', '<', '=', '>', '?', */
+	    60,  61, 255, 255, 255, 255, 255, 255,
 
-	/* '[', '\', ']', '^', '_', '`', 'a', 'b', */ 
-	    -1,  -1,  -1,  -1,  -1,  -1,  26,  27,
+	/* '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', */
+	   255,   0,   1,  2,   3,   4,   5,    6,
 
-	/* 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', */ 
-	    28,  29,  30,  31,  32,  33,  34,  35,
+	/* 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', */
+	     7,   8,   9,  10,  11,  12,  13,  14,
 
-	/* 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', */
-	    36,  37,  38,  39,  40,  41,  42,  43,
+	/* 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', */
+	    15,  16,  17,  18,  19,  20,  21,  22,
 
-	/* 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', */
-	    44,  45,  46,  47,  48,  49,  50,  51,
+	/* 'X', 'Y', 'Z', '[', '\', ']', '^', '_', */
+	    23,  24,  25, 255, 255, 255, 255, 255,
+
+	/* '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', */
+	   255,  26,  27,  28,  29,  30,  31,  32,
+
+	/* 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', */
+	    33,  34,  35,  36,  37,  38,  39,  40,
+
+	/* 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', */
+	    41,  42,  43,  44,  45,  46,  47,  48,
+
+	/* 'x', 'y', 'z', '{', '|', '}', '~', del, */
+	    49,  50,  51, 255, 255, 255, 255, 255
 };
 
-int
+unsigned int
 base64_encode(const unsigned char *in, unsigned int inlen, char *out)
 {
-	unsigned int i, j;
+	int s;
+	unsigned int i;
+	unsigned int j;
+	unsigned char c;
+	unsigned char l;
 
+	s = 0;
+	l = 0;
 	for (i = j = 0; i < inlen; i++) {
-		int s = i % 3; 			/* from 6/gcd(6, 8) */
+		c = in[i];
 
 		switch (s) {
 		case 0:
-			out[j++] = base64en[(in[i] >> 2) & 0x3F];
-			continue;
+			s = 1;
+			out[j++] = base64en[(c >> 2) & 0x3F];
+			break;
 		case 1:
-			out[j++] = base64en[((in[i-1] & 0x3) << 4) + ((in[i] >> 4) & 0xF)];
-			continue;
+			s = 2;
+			out[j++] = base64en[((l & 0x3) << 4) | ((c >> 4) & 0xF)];
+			break;
 		case 2:
-			out[j++] = base64en[((in[i-1] & 0xF) << 2) + ((in[i] >> 6) & 0x3)];
-			out[j++] = base64en[in[i] & 0x3F];
+			s = 0;
+			out[j++] = base64en[((l & 0xF) << 2) | ((c >> 6) & 0x3)];
+			out[j++] = base64en[c & 0x3F];
+			break;
 		}
+		l = c;
 	}
 
-	/* move back */
-	i -= 1;
-
-	/* check the last and add padding */
-	if ((i % 3) == 0) {
-		out[j++] = base64en[(in[i] & 0x3) << 4];
+	switch (s) {
+	case 1:
+		out[j++] = base64en[(l & 0x3) << 4];
 		out[j++] = BASE64_PAD;
 		out[j++] = BASE64_PAD;
-	} else if ((i % 3) == 1) {
-		out[j++] = base64en[(in[i] & 0xF) << 2];
+		break;
+	case 2:
+		out[j++] = base64en[(l & 0xF) << 2];
 		out[j++] = BASE64_PAD;
+		break;
 	}
 
 	out[j] = 0;
 
-	return BASE64_OK;
+	return j;
 }
 
-int
+unsigned int
 base64_decode(const char *in, unsigned int inlen, unsigned char *out)
 {
-	unsigned int i, j;
+	unsigned int i;
+	unsigned int j;
+	unsigned char c;
 
-	for (i = j = 0; i < inlen; i++) {
-		int c;
-		int s = i % 4; 			/* from 8/gcd(6, 8) */
-
-		if (in[i] == '=')
-			return BASE64_OK;
-
-		if (in[i] < BASE64DE_FIRST || in[i] > BASE64DE_LAST ||
-		    (c = base64de[in[i] - BASE64DE_FIRST]) == -1)
-			return BASE64_INVALID;
-
-		switch (s) {
-		case 0:
-			out[j] = ((unsigned int)c << 2) & 0xFF;
-			continue;
-		case 1:
-			out[j++] += ((unsigned int)c >> 4) & 0x3;
-
-			/* if not last char with padding */
-			if (i < (inlen - 3) || in[inlen - 2] != '=')
-				out[j] = ((unsigned int)c & 0xF) << 4; 
-			continue;
-		case 2:
-			out[j++] += ((unsigned int)c >> 2) & 0xF;
-
-			/* if not last char with padding */
-			if (i < (inlen - 2) || in[inlen - 1] != '=')
-				out[j] =  ((unsigned int)c & 0x3) << 6;
-			continue;
-		case 3:
-			out[j++] += (unsigned char)c;
-		}
+	if (inlen & 0x3) {
+		return 0;
 	}
 
-	return BASE64_OK;
-}
+	for (i = j = 0; i < inlen; i++) {
+		if (in[i] == BASE64_PAD) {
+			break;
+		}
+		if (in[i] < 0) {
+			return 0;
+		}
 
+		c = base64de[in[i]];
+		if (c == 255) {
+			return 0;
+		}
+
+		switch (i & 0x3) {
+		case 0:
+			out[j] = (c << 2) & 0xFF;
+			break;
+		case 1:
+			out[j++] |= (c >> 4) & 0x3;
+			out[j] = (c & 0xF) << 4; 
+			break;
+		case 2:
+			out[j++] |= (c >> 2) & 0xF;
+			out[j] = (c & 0x3) << 6;
+			break;
+		case 3:
+			out[j++] |= c;
+			break;
+		}
+	}
+	out[j] = '\0';
+
+	return j;
+}
--- base64.h
+++ base64.h
@@ -1,17 +1,20 @@
-#ifndef __BASE64_H__
-#define __BASE64_H__
+#ifndef BASE64_H
+#define BASE64_H
 
-enum {BASE64_OK = 0, BASE64_INVALID};
+#define BASE64_ENCODE_OUT_SIZE(s) ((unsigned int)((((s) + 2) / 3) * 4 + 1))
+#define BASE64_DECODE_OUT_SIZE(s) ((unsigned int)(((s) / 4) * 3))
 
-#define BASE64_ENCODE_OUT_SIZE(s)	(((s) + 2) / 3 * 4)
-#define BASE64_DECODE_OUT_SIZE(s)	(((s)) / 4 * 3)
-
-int
+/*
+ * out is null-terminated encode string.
+ * return values is out length, exclusive terminating `\0'
+ */
+unsigned int
 base64_encode(const unsigned char *in, unsigned int inlen, char *out);
 
-int
+/*
+ * return values is out length
+ */
+unsigned int
 base64_decode(const char *in, unsigned int inlen, unsigned char *out);
 
-
-#endif /* __BASE64_H__ */
-
+#endif /* BASE64_H */
